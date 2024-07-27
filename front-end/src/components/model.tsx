15f36d016acf945ebd3393cf9cd2f1347c0d721f
CHIRAG DALMIA <119760552+ChiragDalmia@users.noreply.github.com>
2024-07-27 15:56:33 -0400

    added spline lesssggoooooo

---
+++ front-end/src/components/model.tsx
@@ -0,0 +1,13 @@
+import Spline from "@splinetool/react-spline";
+
+import React from "react";
+
+const Model = () => {
+  return (
+    <div className="border h-screen">
+      <Spline scene="https://prod.spline.design/o1ONSc8QZYOX9Hwg/scene.splinecode" />
+    </div>
+  );
+};
+
+export default Model;
