64952d32ed254eed1ff3fa3d2fbdebbef3a472fd
James Cao <jamescao800@gmail.com>
2024-07-27 16:28:30 -0400

    made the gemini thing work - James C

---
+++ front-end/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import Model from "@/components/model";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Model />
+    </main>
+  );
+}
--- test.py
+++ test.py
@@ -5,12 +5,27 @@
 # screenshot_frame = ImageGrab.grab(bbox=(100,100,500,500))
 
 # screenshot_frame.save("screenshot.png")
+
+# do pip install -q -U google-generativeai
+
 import google.generativeai as genai
+import os 
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_keysadf = os.getenv("api_key") 
+
+print(api_keysadf)
+
+genai.configure(api_key=api_keysadf)
 
 
 # upload the photo
-frame = genai.upload_file(path="screenshot.png", display_name="screenshot")
+frame = genai.upload_file(path="image.png", display_name="screenshot")
 print("file compeletly uploaded")
+
 
 
 # see if the file was stored correctly 
@@ -24,6 +39,11 @@ except:
 model = genai.GenerativeModel(model_name="gemini-1.5-pro")
 
 # Prompt the model with text and the previously uploaded image.
-response = model.generate_content([frame, "Describe how this product might be manufactured."])
+response = model.generate_content([frame, "The two people are having a dance competition. The person on the left is leading and the person on the right is trying to follow along. Give a number from 0-10 on how well the person on the right is following along. Make assumptions if necessary. Also can you provide a bit of feedback to the person on the right on how they can fix their position to match the leader. Give us a JSON object with ONLY this structure: '{'score': [score], 'feedback': [feedback]}.' No explanation is needed and the word json does not need to be mentioned."])
 
-print(response.text)
+# Delete them after use.
+genai.delete_file(frame.name)
+print(f'Deleted file {frame.uri}')
+
+
+print(f"{json.loads(response.text.replace("'", '"'))}")
